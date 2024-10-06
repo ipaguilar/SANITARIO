1b5be7ba471d95e2e5d4787b1c282dccd3d2c979
Candida Licia Choque Machaca <candida.choque.machaca.dat@gmail.com>
2024-10-06 19:13:57 -0400

    Creacion de DW

---
+++ SANITARIO_DW/Schema/Tables/FacInternacion.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[FacInternacion] (
+    [Id_FacInternacion] INT IDENTITY(1,1) NOT NULL,
+    [ID_Paciente] INT NOT NULL,
+    [ID_Hospital] INT NOT NULL,
+    [ID_Medico] INT NOT NULL,
+    [ID_Fecha] INT NOT NULL,
+	[Diagnostico] FLOAT NULL,
+	[datos_adicionales] FLOAT NULL,
+	[Id_Cama] INT NOT NULL,
+	[Es_Internacion] Int NULL,
+    [Fecha_Ingreso] DATETIME NOT NULL,
+    [Fecha_Salida] DATETIME NOT NULL,
+	[Precio_total] FLOAT NULL,
+    [RowVersion] INT NOT NULL,	
+    CONSTRAINT [PK_Internacion_FacInternacion] PRIMARY KEY CLUSTERED ([Id_FacInternacion] ASC),
+	CONSTRAINT [FK_Internacion_Paciente] FOREIGN KEY ([ID_Paciente]) REFERENCES [dbo].[DimPaciente] ([Id_Paciente]),
+	CONSTRAINT [FK_Internacion_Hospital] FOREIGN KEY ([Id_Hospital]) REFERENCES [dbo].[DimHospital] ([Id_DimHospital]),
+	CONSTRAINT [FK_Internacion_Medico] FOREIGN KEY ([Id_Medico]) REFERENCES [dbo].[DimMedico] ([Id_Medico]),
+	CONSTRAINT [FK_Internacion_Fecha] FOREIGN KEY ([Id_Fecha]) REFERENCES [dbo].[DimTiempo] ([Id_Tiempo])
+	);
---
+++ SANITARIO_DW/Schema/Tables/FacVisita.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[FacVisita] (
+    [Id_FacVisita] INT IDENTITY(1,1) NOT NULL,
+    [Id_Paciente] INT NOT NULL,
+    [Id_Hospital] INT NOT NULL,
+    [Id_Medico] INT NOT NULL,
+    [Id_Visita] INT NOT NULL,
+    [Id_Fecha] INT NOT NULL,
+    [Precio_total] FLOAT NULL,
+	[es_Reconsulta] int NULL,
+    [Id_Visita_inicial] INT NOT NULL,
+	[Diagnostico] FLOAT NULL,
+	[datos_adicionales] FLOAT NULL,
+    [RowVersion] INT NOT NULL,
+    CONSTRAINT [PK_Visita_FacVisita] PRIMARY KEY CLUSTERED ([Id_FacVisita] ASC),
+    CONSTRAINT [FK_Visita_Paciente] FOREIGN KEY ([Id_Paciente]) REFERENCES [dbo].[DimPaciente] ([Id_Paciente]),
+	CONSTRAINT [FK_Visita_Hospital] FOREIGN KEY ([Id_Hospital]) REFERENCES [dbo].[DimHospital] ([Id_DimHospital]),
+	CONSTRAINT [FK_Visita_Medico] FOREIGN KEY ([Id_Medico]) REFERENCES [dbo].[DimMedico] ([Id_Medico]),
+	CONSTRAINT [FK_Visita_Tiempo] FOREIGN KEY ([Id_Fecha]) REFERENCES [dbo].[DimTiempo] ([Id_Tiempo])
+);
