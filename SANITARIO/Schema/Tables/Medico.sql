1eac2abfb9d2de88a0b3bbfff7ba9e7708edd959
Codxel <weimar.ustarezrivera@gmail.com>
2024-10-08 18:41:52 -0400

    Fix a tabla medico

--- SANITARIO/Schema/Tables/Medico.sql
+++ SANITARIO/Schema/Tables/Medico.sql
@@ -4,5 +4,6 @@
     [Nombre] VARCHAR(50) NOT NULL,
     [Apellidos] VARCHAR(50) NOT NULL,
     [FechaNacimiento] DATETIME NOT NULL,
+	[rowversion] [timestamp] NOT NULL,
     CONSTRAINT [PK_Medico] PRIMARY KEY CLUSTERED ([Id_Medico] ASC)
 );
