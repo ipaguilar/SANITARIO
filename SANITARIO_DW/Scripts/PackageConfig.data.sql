6b0fb274d1c0c9fe21c1a4ebe861d5139207d1bd
Codxel <weimar.ustarezrivera@gmail.com>
2024-10-08 20:06:43 -0400

    corrección en script packageconfig

--- SANITARIO_DW/Scripts/PackageConfig.data.sql
+++ SANITARIO_DW/Scripts/PackageConfig.data.sql
@@ -2,20 +2,20 @@
               FROM [dbo].[PackageConfig]
 			  WHERE [TableName] = 'Hospital')
  BEGIN
-	INSERT [dbo].[PackageConfig] ([TableName], [LastRowVersion]) VALUES ('Customer', 0)
+	INSERT [dbo].[PackageConfig] ([TableName], [LastRowVersion]) VALUES ('Hospital', 0)
  END
 GO
 IF NOT EXISTS(SELECT TOP(1) 1
               FROM [dbo].[PackageConfig]
 			  WHERE [TableName] = 'Medico')
  BEGIN
-	INSERT [dbo].[PackageConfig] ([TableName], [LastRowVersion]) VALUES ('Staff', 0)
+	INSERT [dbo].[PackageConfig] ([TableName], [LastRowVersion]) VALUES ('Medico', 0)
  END
 GO
 IF NOT EXISTS(SELECT TOP(1) 1
               FROM [dbo].[PackageConfig]
 			  WHERE [TableName] = 'Paciente')
  BEGIN
-  INSERT [dbo].[PackageConfig] ([TableName], [LastRowVersion]) VALUES ('Store', 0)
+  INSERT [dbo].[PackageConfig] ([TableName], [LastRowVersion]) VALUES ('Paciente', 0)
  END
 GO
